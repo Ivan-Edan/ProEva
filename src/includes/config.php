95df4ca54495e7bc9c593bcce44919c2c6d6bf3d
PeterPaulSaul <paulsaul0909@gmail.com>
2025-01-06 22:58:39 +0800

    hello

--- src/includes/config.php
+++ src/includes/config.php
@@ -4,7 +4,7 @@
 $servername = "localhost"; 
 $username = 'root'; 
 $password = ''; 
-$dbname = "db";
+$dbname = "db_proeva";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
